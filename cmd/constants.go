013ee8c65db7a038858b12713fa933712ac907dc
Frode Eriksen <frode@eriksen.casa>
2025-12-17 22:09:49 +0100

    Added Timeout to Splunk and A / AAAA by choice

--- cmd/constants.go
+++ cmd/constants.go
@@ -3,6 +3,7 @@ package main
 const (
 	DefaultDescription   = "DNS Benchmark"         // Default benchmark name
 	DefaultDomain        = "example.com"           // Domain to query
+	DefaultDNSType       = "A"                     // DNS record type to query (A or AAAA)
 	DefaultDuration      = 10                      // Seconds
 	DefaultQPS           = 50                      // Queries per second per server
 	DefaultTimeout       = 1500                    // Milliseconds
